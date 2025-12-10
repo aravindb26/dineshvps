ef38252b3ba16f5f0db2c43fd9cac7ed45262c60
ARAVIND <89305482+aravindb26@users.noreply.github.com>
2025-12-10 16:51:46 +0530

    bot.sh

--- ygsh/bot/bot.sh
+++ ygsh/bot/bot.sh
@@ -1,24 +1,25 @@
 while true;
-   do subjack -w domain.txt | notify --silent -p discord -id "crawl8"; 
+   do subfinder -silent -dL domain.txt -all | sort -u | anew subdomains.txt
+      subjack -w subdomains.txt | notify --silent -p discord -id "crawl8";
       nuclei -up | notify --silent -p discord -id "crawl8"
       printf "Updating templates :D" | notify --silent -p discord -id "crawl8"
       nuclei --update-templates --silent
       printf "Templates are updated... Let's go..." | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/cves/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/default-logins/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/network/exposures/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/misconfiguration/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/takeovers/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/vulnerabilities/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/exposed-panels/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/headless/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/workflows/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/cnvd/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/technologies/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/fuzzing/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/network/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/dns/ | notify --silent -p discord -id "crawl8"
-      nuclei -l domain.txt -t $HOME/nuclei-templates/http/misconfiguration/jenkins/ | notify --silent -p discord -id "crawl8"
-      cat domain.txt | waybackurls | httpx -mc 200 -ct | grep application/json | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/cves/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/default-logins/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/network/exposures/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/misconfiguration/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/takeovers/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/vulnerabilities/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/exposed-panels/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/headless/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/workflows/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/cnvd/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/technologies/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/fuzzing/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/network/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/dns/ -es info | notify --silent -p discord -id "crawl8"
+      nuclei -l subdomains.txt -t $HOME/nuclei-templates/http/misconfiguration/jenkins/ -es info | notify --silent -p discord -id "crawl8"
+      cat subdomains.txt | waybackurls | httpx -mc 200 -ct | grep application/json | notify --silent -p discord -id "crawl8"
    sleep 7200;
 done
